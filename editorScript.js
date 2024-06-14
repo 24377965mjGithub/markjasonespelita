e0acf0513d42a7a2537d129a20dd1c10d5d0f74f
Mark Jason Espelita <mark.medivadigital@gmail.com>
2024-06-14 20:18:35 +0800

    Editor

---
+++ editorScript.js
@@ -0,0 +1,44 @@
+const htmlEditor = document.getElementById('html-editor');
+const cssEditor = document.getElementById('css-editor');
+const jsEditor = document.getElementById('js-editor');
+const preview = document.getElementById('preview');
+
+htmlEditor.addEventListener('input', updatePreview);
+cssEditor.addEventListener('input', updatePreview);
+jsEditor.addEventListener('input', updatePreview);
+
+let storage = JSON.parse(localStorage.getItem('CodeStorage'));
+
+// populate inputs/textboxes
+htmlEditor.value = storage.html;
+cssEditor.value = storage.css;
+jsEditor.value = storage.js;
+
+const combinedCode = `${storage.html}<style>${storage.css}</style><script>${storage.js}</script>`;
+preview.srcdoc = combinedCode;
+
+function updatePreview() {
+  const html = htmlEditor.value;
+  const cssRaw = cssEditor.value;
+  const jsRaw = jsEditor.value;
+
+  // storage
+  let storage = JSON.parse(localStorage.getItem('CodeStorage'));
+  if (!storage) {
+    storage = {
+      html: '',
+      css: '',
+      js: ''
+    };
+  }
+
+  storage.html = html;
+  storage.css = cssRaw;
+  storage.js = jsRaw;
+
+  localStorage.setItem('CodeStorage', JSON.stringify(storage));
+
+  const combinedCode = `${storage.html}<style>${storage.css}</style><script>${storage.js}</script>`;
+
+  preview.srcdoc = combinedCode;
+}
