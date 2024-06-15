b0f00eb41ebe9ca415b53fc3fbd06923b2d4d010
Mark Jason Espelita <mjespelita.swashed@gmail.com>
2024-06-15 11:04:03 +0800

    v1.0.2

--- editorScript.js
+++ editorScript.js
@@ -1,11 +1,15 @@
 const htmlEditor = document.getElementById('html-editor');
 const cssEditor = document.getElementById('css-editor');
 const jsEditor = document.getElementById('js-editor');
+const headerEditor = document.getElementById('header-editor');
+const footerEditor = document.getElementById('footer-editor');
 const preview = document.getElementById('preview');
 
 htmlEditor.addEventListener('input', updatePreview);
 cssEditor.addEventListener('input', updatePreview);
 jsEditor.addEventListener('input', updatePreview);
+headerEditor.addEventListener('input', updatePreview);
+footerEditor.addEventListener('input', updatePreview);
 
 let storage = JSON.parse(localStorage.getItem('CodeStorage'));
 
@@ -13,7 +17,9 @@ if (!storage) {
   storage = {
     html: '\n\n',
     css: '\n\n',
-    js: '\n\n'
+    js: '\n\n',
+    header: '',
+    footer: '',
   };
 }
 
@@ -21,32 +27,68 @@ if (!storage) {
 htmlEditor.value = storage.html;
 cssEditor.value = storage.css;
 jsEditor.value = storage.js;
+headerEditor.value = storage.header;
+footerEditor.value = storage.footer;
 
-const combinedCode = `${storage.html}<style>${storage.css}</style><script>${storage.js}</script>`;
+// code structure -----
+
+const combinedCode = `
+    ${storage.header}
+    <style>${storage.css}</style>
+
+    ${storage.html}
+
+    ${storage.footer}
+    <script>${storage.js}</script>
+  `;
+
+// end code structure -----
+
 preview.srcdoc = combinedCode;
 
 function updatePreview() {
-  const html = htmlEditor.value;
-  const cssRaw = cssEditor.value;
-  const jsRaw = jsEditor.value;
+    const html = htmlEditor.value;
+    const cssRaw = cssEditor.value;
+    const jsRaw = jsEditor.value;
+    const header = headerEditor.value;
+    const footer = footerEditor.value;
 
-  // storage
-  let storage = JSON.parse(localStorage.getItem('CodeStorage'));
-  if (!storage) {
-    storage = {
-      html: '\n\n',
-      css: '\n\n',
-      js: '\n\n'
-    };
-  }
+    // storage
+    let storage = JSON.parse(localStorage.getItem('CodeStorage'));
+    if (!storage) {
+      storage = {
+        html: '\n\n',
+        css: '\n\n',
+        js: '\n\n',
+        header: '',
+        footer: '',
+      };
+    }
 
-  storage.html = html;
-  storage.css = cssRaw;
-  storage.js = jsRaw;
+    storage.html = html;
+    storage.css = cssRaw;
+    storage.js = jsRaw;
+    storage.header = header;
+    storage.footer = footer;
 
-  localStorage.setItem('CodeStorage', JSON.stringify(storage));
 
-  const combinedCode = `${storage.html}<style>${storage.css}</style><script>${storage.js}</script>`;
+    localStorage.setItem('CodeStorage', JSON.stringify(storage));
 
-  preview.srcdoc = combinedCode;
+    // const combinedCode = `${storage.html}<style>${storage.css}</style><script>${storage.js}</script>`;
+
+    // code structure -----
+
+    const combinedCode = `
+      ${storage.header}
+      <style>${storage.css}</style>
+
+      ${storage.html}
+
+      ${storage.footer}
+      <script>${storage.js}</script>
+    `;
+
+    // end code structure -----
+
+    preview.srcdoc = combinedCode;
 }
